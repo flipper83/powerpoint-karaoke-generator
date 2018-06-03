4769ae86936250b5dfcf7c0184de5f66836e2c4f
Jorge Juan Barroso Carmona <jorge@karumi.com>
2018-06-03 17:34:14 +0200

    add reddit image provider.

--- powerpointkaraoke/image_provider.py
+++ powerpointkaraoke/image_provider.py
@@ -1,3 +1,24 @@
+import random
+import urllib.request
+
+import praw
+
+
 class ImageProvider(object):
+    reddit = None
+
+    def __init__(self):
+        self.reddit = praw.Reddit(client_id='YKo_9gh5XpDJYQ',
+                     client_secret= None,
+                     redirect_uri='http://localhost:8080',
+                     user_agent='karaoke')
+        self.reddit.read_only = True
+
     def next_image(self):
+        images = []
+        for submission in self.reddit.subreddit('Funnypics').new():
+            images.append(submission.preview['images'][0]['source']['url'])
+
+        image_to_download = random.choice(images)
+        urllib.request.urlretrieve(image_to_download, "image.jpg")
         return "image.jpg"
